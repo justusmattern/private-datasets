c72056677020c12bd7bcd0f79a4ec9ca53d2e4d8
Justus Mattern <justus_mattern@gmx.de>
2022-04-29 02:48:06 +0200

    generation script

---
+++ generate.py
@@ -0,0 +1,52 @@
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import os
+import sys
+import argparse
+
+def generate(model, tokenizer, prompts, num_sequences_per_prompt, filenames):
+
+    tokenizer = GPT2Tokenizer.from_pretrained(tokenizer)
+    model = GPT2LMHeadModel.from_pretrained(model, pad_token_id=tokenizer.eos_token_id)
+    model.parallelize()
+
+    for prompt, num_sequences, file in zip(prompts, num_sequences_per_prompt, filenames):
+
+        input_ids = tokenizer.encode(prompt + ' ', return_tensors='pt')
+        final_samples = []
+
+        while len(final_samples) < num_sequences:
+
+            sample_outputs = model.generate(
+                input_ids,
+                do_sample=True, 
+                max_length=1024, 
+                top_k=20, 
+                top_p=0.97, 
+                num_return_sequences=10
+            )
+
+            
+            for sample in sample_outputs:
+                text = tokenizer.decode(sample, skip_special_tokens = False)
+                text = text.split(tokenizer.eos_token)[0]
+                final_samples.append(text)
+        
+        with open(file, 'w') as f:
+            for sample in final_samples:
+                f.write(sample.replace('\n', ' ')+'\n')
+        
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--model', type=str)
+    parser.add_argument('--tokenizer')
+    parser.add_argument('--prompts', type=str, nargs='+')
+    parser.add_argument('--num-sequences-per-prompt', type=int, nargs='+')
+    parser.add_argument('--filenames', type=str, nargs='+')
+
+    args = parser.parse_args()
+
+    generate(*args)
+
--- train_lm.py
+++ train_lm.py
@@ -46,24 +46,25 @@ def pre_process(texts, labels):
 
 def run(args):
     model = GPT2LMHeadModel.from_pretrained(args.model)
-    #model.parallelize()
+    model.parallelize()
+    model.train()
     tokenizer = GPT2Tokenizer.from_pretrained(args.tokenizer)
     tokenizer.pad_token = tokenizer.eos_token
     optimizer = torch.optim.Adam(model.parameters(), lr = 1e-4)
     loss_fn = torch.nn.CrossEntropyLoss(reduction="none")
 
-    #privacy_engine = PrivacyEngine(
-    #    model,
-    #    batch_size=args.batch_size,
-    #    sample_size=1024,
-    #    epochs=args.epochs,
-    #    max_grad_norm=0.1,
-    #    target_epsilon=3,
-    #)
+    privacy_engine = PrivacyEngine(
+        model,
+        batch_size=args.batch_size,
+        sample_size=1024,
+        epochs=args.epochs,
+        max_grad_norm=0.1,
+        target_epsilon=3,
+    )
 
-    #privacy_engine.attach(optimizer)
+    privacy_engine.attach(optimizer)
 
-    train_texts, train_labels = get_data('imdb/imdb_train.txt')
+    train_texts, train_labels = get_data('imdb/imdb_train_head.txt')
     train_data = Dataset(train_texts, train_labels, tokenizer.eos_token)
 
     train_loader = torch.utils.data.DataLoader(train_data, shuffle=False, batch_size=args.batch_size)
@@ -72,20 +73,26 @@ def run(args):
         print(f'training epoch {epoch}')
 
         total_loss = 0
+        iter = 0
         for texts, labels in train_loader:
-            
+            iter += 1
+            if iter % 100 == 0:
+                print(iter)
             prompts, total_texts = pre_process(texts, labels)
-            tokenized_prompts = tokenizer(prompts, truncation=True, max_length=1024, return_tensors='pt').input_ids#.to('cuda:0')
-            tokenized_texts = tokenizer(total_texts, truncation=True, max_length=1024, return_tensors='pt', padding=True).input_ids#.to('cuda:0')
+            tokenized_prompts = tokenizer(prompts, truncation=True, max_length=1024, return_tensors='pt').input_ids.to('cuda:0')
+            tokenized_texts = tokenizer(total_texts, truncation=True, max_length=1024, return_tensors='pt', padding=True).input_ids.to('cuda:0')
 
             logits = model(tokenized_texts, labels=tokenized_texts).logits # - model(tokenized_prompts, labels=tokenized_text).loss*len()
-            print(logits.shape)
-            print(labels.shaoe)
-            lm_loss = 0
+            #print(logits.shape)
+            #print(tokenized_texts.shape)
+            lm_loss = loss_fn(logits.permute(0,2,1), tokenized_texts).mean(dim=1)
+            #print(lm_loss)
+            #print(lm_loss.shape)
             optimizer.step(loss=lm_loss)
-            total_loss += lm_loss.item()
+            total_loss += lm_loss.sum().item()
 
         print('total lm loss', total_loss/len(train_data))
+        model.save_pretrained(f'gpt2_epoch{epoch}')
 
 
 if __name__=='__main__':
